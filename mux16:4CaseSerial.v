fcc39c40575bccae74279e5806a9f12973f8521c
Ramon Silva <ramondecerqueirasilva@gmail.com>
2018-12-30 16:18:32 -0300

    Update mux16:4CaseSerial.v

--- mux16:4CaseSerial.v
+++ mux16:4CaseSerial.v
@@ -1,24 +1,19 @@
-module mux16for4(
-	data0[3:0],data1[3:0],data2[3:0],data3[3:0],
-	selector[3:0],
-	y[3:0]
-);
+module mux16for4(data0,data1,data2,data3,selector,y);
 
-input data0,data1,data2,data3;
-input selector;
-output y;
-reg y; 	//case sensitive
+input [3:0]data0,[3:0]data1,[3:0]data2,[3:0]data3;
+input [1:0]selector;
+output [3:0]y;
+reg [3:0]y; 	//case sensitive
 	
-always@(selector or da or b or c or d){
-	begin
-		case(selector)
-			2'b00:y0 = a0|b0|c0|d0;
-			2'b01:y1 = a1|b1|c1|d1;
-			2'b10:y2 = a2|b2|c2|d2;
-			defaut
-				  y3 = a3|b3|c3|d3;
-		endcase
-	end
-}
+always@(selector or data0 or data1 or data2 or data3)
+begin
+	case(selector)
+		2'b00:	y = data0;
+		2'b01:	y = data1;
+		2'b10:	y = data2;
+		default
+				y = data3;
+	endcase
+end
 
 endmodule
