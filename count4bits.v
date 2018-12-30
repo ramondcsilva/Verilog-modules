662052cfcb4c482d79c80bc24d6064e95f98f0ee
Ramon Silva <ramondecerqueirasilva@gmail.com>
2018-12-30 18:06:07 -0300

    Update and rename count9bits.v to count4bits.v

---
+++ count4bits.v
@@ -0,0 +1,20 @@
+////Autorship: Ramon de Cerqueira Silva
+//Description: Simple 4 bit counter with enable, reset.
+
+module count4bits(y,clear,enable,clock);
+
+input clock,enable,clear;
+output [3:0]y;
+reg [3:0]y;
+
+always@(posedge clock)
+if(clear)
+begin
+  y = 4'b0;
+end  
+else if(enable)
+begin  
+  y <= y + 1;
+end  
+
+endmodule
